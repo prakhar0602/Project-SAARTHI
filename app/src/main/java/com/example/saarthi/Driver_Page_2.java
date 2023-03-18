adf7624cb8efd97f7513aece091438f67a53c7ba
Prakhar Srivastava <prakhar.srivastava_cs.aiml21@gla.ac.in>
2023-03-18 12:40:00 +0530

    Login+registeration

---
+++ app/src/main/java/com/example/saarthi/Driver_Page_2.java
@@ -0,0 +1,13 @@
+package com.example.saarthi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Driver_Page_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_page2);
+    }
+}
